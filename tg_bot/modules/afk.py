5bac80e152c4b490b4f5c1e24972ca635335b2f6
abhishek335 <62733015+abhishek335@users.noreply.github.com>
2020-05-28 16:52:54 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -82,7 +82,7 @@ def __gdpr__(user_id):
 
 
 __help__ = """
- - /afk <reason>: mark yourself as AFK(away from keyboard).
+ - /afk <reason>: mark yourself as AFK.
  - brb <reason>: same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
