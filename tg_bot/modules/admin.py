96a5c0f3411f7e35779985f0b0122becf78bad6e
abhishek335 <62733015+abhishek335@users.noreply.github.com>
2020-06-13 15:33:56 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -334,18 +334,6 @@ def adminlist(bot: Bot, update: Update):
         if status == "creator":
             text += "\n 🔱 Creator:"
             text += "\n` • `{} \n\n • *Administrators*:".format(name)
-    for admin in administrators:
-        user = admin.user
-        status = admin.status
-        chat = update.effective_chat
-        count = chat.get_members_count()
-        name = "[{}](tg://user?id={})".format(user.first_name + " " + (user.last_name or ""), user.id)
-        if user.username:
-            name = escape_markdown("@" + user.username)
-            
-        if status == "administrator":
-            text += "\n`👮🏻 `{}".format(name)
-            members = "\n\n*Members:*\n`🙍‍♂️ ` {} users".format(count)
             
     msg.reply_text(text + members, parse_mode=ParseMode.MARKDOWN)
 
